54edcf488fff597fe09f51b88ed90a0fff6e0c82
bugzweiser <bugzweiser@gmail.com>
2019-06-20 05:12:35 -0700

    basic gameplay complete

--- assets/js/app.js
+++ assets/js/app.js
@@ -14,4 +14,91 @@ $(document).ready(function() {
     firebase.initializeApp(config);
     var dataRef = firebase.database();
 
+    //game stats
+    var wins = 0;
+    var losses = 0;
+    var ties = 0;
+
+    document.onkeyup = function(event) {
+
+        //gameplay variables / choices
+        var userGuess = event.key;
+        var computerChoices = ["r", "p", "s"];
+        var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+
+        //player image population
+        var rockImg = "assets/images/rock.jpg";
+        var paperImg = "assets/images/paper.jpg";
+        var scissorsImg = "assets/images/scissors.jpg";
+
+        var rock = $("<img>");
+        rock.attr('src', rockImg);
+        rock.attr("alt", "rock.jpg");
+
+        var paper = $("<img>");
+        paper.attr('src', paperImg);
+        paper.attr("alt", "paper.jpg");
+
+        var scissors = $("<img>");
+        scissors.attr('src', scissorsImg);
+        scissors.attr("alt", "scissors.jpg");
+
+
+        //debugging
+        console.log("Letter picked: " + userGuess);
+        console.log("Computer picked: " + computerGuess);
+
+
+        // local image population
+        if (userGuess === "r") {
+            $("#player_img").empty();
+            $("#player_img").append(rock);
+        }
+        if (userGuess === "p") {
+            $("#player_img").empty();
+            $("#player_img").append(paper);
+        }
+        if (userGuess === "s") {
+            $("#player_img").empty();
+            $("#player_img").append(scissors);
+        }
+
+        //remote image population
+        if (computerGuess === "r") {
+            $("#remote_img").empty();
+            $("#remote_img").append(rock);
+        }
+        if (computerGuess === "p") {
+            $("#remote_img").empty();
+            $("#remote_img").append(paper);
+        }
+        if (computerGuess === "s") {
+            $("#remote_img").empty();
+            $("#remote_img").append(scissors);
+        }
+
+        // This logic determines the outcome of the game (win/loss/tie), and increments the appropriate number
+        if ((userGuess === "r") || (userGuess === "p") || (userGuess === "s")) {
+
+            if ((userGuess === "r" && computerGuess === "s") ||
+                (userGuess === "s" && computerGuess === "p") ||
+                (userGuess === "p" && computerGuess === "r")) {
+                wins++;
+            } else if (userGuess === computerGuess) {
+                ties++;
+            } else {
+                losses++;
+            }
+
+            //debug game stats
+            console.log("Wins: " + wins);
+            console.log("Losses: " + losses);
+            console.log("ties: " + ties);
+
+            //game stats post
+            $(".win").text(wins);
+            $(".loss").text(losses);
+            $(".tie").text(ties);
+        }
+    };
 });
